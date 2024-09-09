225e5634999b64b98f91fd7dafa67c00ea842220
Sessiom <caufaglj2@newpaltz.edu>
2024-09-09 11:17:25 -0400

    move_and_slide()

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,8 +1,14 @@
 extends CharacterBody2D
 
+@export var speed : float = 200.0
+
 @onready var animation_tree : AnimationTree = $AnimationTree
 @onready var sprite : Sprite2D = $body
-var direction : Vector2 = Vector2(0,0)
+@onready var state_machine : playerStateMachine = $playerStateMachine
+
+var direction : Vector2 = Vector2.ZERO
+
+
 
 func _ready():
 	animation_tree.active = true
@@ -10,8 +16,18 @@ func _ready():
 func _physics_process(delta):
 	direction = Input.get_vector("left", "right", "up", "down")
 	
+	#control whether to move or not
+	if direction != Vector2.ZERO && state_machine.check_if_can_move():
+		velocity = direction * speed
+	else:
+		velocity = Vector2.ZERO
+		#velocity.x = move_toward(velocity.x, 0, speed)
+		#velocity.y = move_toward(velocity.y, 0, speed)
+		
+	
 	update_animation()
 	update_facing_direction()
+	move_and_slide()
 	
 func update_animation():
 	animation_tree.set("parameters/move/blend_position", direction)
--- scripts/walkState.gd
+++ scripts/walkState.gd
@@ -1,1 +1,3 @@
 extends State
+
+class_name walkState
